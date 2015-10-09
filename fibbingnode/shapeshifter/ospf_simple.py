1b49cae371b9ed4725977e4048f5b78900ef5ead
Olivier Tilmans <olivier.tilmans@uclouvain.be>
2015-10-09 12:57:18 +0200

    Removed references/deps to legacy code
    Fixes #1
    
    Signed-off-by: Olivier Tilmans <olivier.tilmans@uclouvain.be>

--- fibbingnode/shapeshifter/ospf_simple.py
+++ fibbingnode/shapeshifter/ospf_simple.py
@@ -1,30 +1,17 @@
 import utils as ssu
 import networkx as nx
-import modified_dijkstra as md
 from fibbingnode import log as logger
-
+import sys
 
 class OSPFSimple(object):
-
-    def get_new_fake_node(self, nh):
-        new_fake_node = "f" + str(self.vnode_id_counter)
-        self.vnode_id_counter += 1
-        self.vnode_ids.add(new_fake_node)
-        return new_fake_node
+    def __init__(self):
+        self.new_edge_weight = 10e4
 
     def add_dest_to_graphs(self, dest, dag):
         if dest not in dag:
             for node in ssu.find_sink(dag):
                 logger.info('Connected %s to %s in the DAG', node, dest)
                 dag.add_edge(node, dest)
-            return True
-        return False
-
-    def remove_added_dest(self, added_edges):
-        node_to_rem = list(self.vnode_ids)
-        added_edges.extend(self.igp_graph.in_edges(node_to_rem, data=True))
-        self.igp_graph.remove_nodes_from(node_to_rem)
-        self.vnode_ids.clear()
 
     def get_fake_lsas(self):
         return self.fake_ospf_lsas
@@ -33,8 +20,8 @@ class OSPFSimple(object):
         req_nhs = dag.successors(node)
         # compute the originals next-hops of the current node
         try:
-            original_nhs = [p.split(' ')[1]
-                            for p in self.igp_paths[node][dest]]
+            original_nhs = [p[1]
+                            for p in self.igp_paths[node][0][dest]]
         except KeyError:
             original_nhs = []
         return req_nhs, original_nhs
@@ -58,24 +45,62 @@ class OSPFSimple(object):
                 return False
         return True
 
+    def complete_dag(self):
+        """Complete the DAG so that missing nodes have their old (or part of)
+        SPT in it"""
+        for n in self.igp_graph:
+            if n in self.dag or n in self.reqs:
+                continue  # n has its SPT instructions or is a destination node
+            for p in self.igp_paths[n][0][self.dest]:
+                for u, v in zip(p[:-1], p[1:]):
+                    v_in_dag = v in self.dag
+                    self.dag.add_edge(u, v)
+                    if v_in_dag:  # we connected u to the new SPT
+                        break
+
     def solve(self, topo, requirement_dags):
-        self.vnode_id_counter = 0
-        self.vnode_ids = set()
         # a list of tuples with info on the node to be attracted,
         # the forwarding address, the cost to be set in the fake LSA,
         # and the respective destinations
         self.fake_ospf_lsas = []
-        added_edges = []
+        self.reqs = requirement_dags
         self.igp_graph = topo
+        self.igp_paths = ssu.all_shortest_paths(self.igp_graph)
         # process input forwarding DAGs, one at the time
         for dest, dag in requirement_dags.iteritems():
             logger.debug('Solving DAG for dest %s', dest)
             self.dest, self.dag = dest, dag
+            self.add_dest_to_graphs(dest, dag)
+            if dest not in topo:
+                sinks = dag.predecessors(dest)
+                for s in sinks:
+                    logger.info('Adding edge (%s, %s) in the graph',
+                             s, self.dest)
+                    topo.add_edge(s, dest, weight=self.new_edge_weight)
+                for n in topo.nodes_iter():
+                    if n == dest:  # dest is a path in itself
+                        self.igp_paths[n] = ([[n]], 0)
+                        continue
+                    paths = []
+                    cost = sys.maxint
+                    for s in sinks:
+                        if s not in self.igp_paths[n][0]:  # no path to sink
+                            continue
+                        c = self.igp_paths[n][1][s]
+                        p = self.igp_paths[n][0][s]
+                        if c < cost:  # new spt
+                            paths = list(ssu.extend_paths_list(p, dest))
+                            cost = c
+                        if c == cost:  # ecmp
+                            paths.extend(ssu.extend_paths_list(p, dest))
+                    if paths:
+                        _t = self.igp_paths[n]
+                        _t[0][dest] = paths
+                        _t[1][dest] = cost
+            self.complete_dag()
             # Add temporarily the destination to the igp graph and/or req dags
             if not self.solvable(dest, dag):
                 continue
-            added = self.add_dest_to_graphs(dest, dag)
-            self.igp_paths = md.all_pairs_dijkstra_ecmp_path(self.igp_graph)
             for node in nx.topological_sort(dag, reverse=True)[1:]:
                 nhs, original_nhs = self.nhs_for(node, dag, dest)
                 if not self.require_fake_node(nhs, original_nhs):
@@ -83,14 +108,8 @@ class OSPFSimple(object):
                     continue
                 for req_nh in nhs:
                     logger.debug('Placing a fake node for nh %s', req_nh)
-                    new_fake_node = self.get_new_fake_node(req_nh)
-                    self.fake_ospf_lsas.append((node, req_nh, -1, dest))
-                    self.igp_graph.add_edges_from(
-                        [(node, new_fake_node, {'weight': -1}),
-                         (new_fake_node, dest, {'weight': -1})])
-            # Remove The destination from the graph otherwise
-            # it could be used as a router node in the next iteration
-            if added:
-                dag.remove_nodes_from([dest])
-            self.remove_added_dest(added_edges)
-        return self.igp_graph
+                    self.fake_ospf_lsas.append(ssu.LSA(node=node,
+                                                       nh=req_nh,
+                                                       cost=-1,
+                                                       dest=dest))
+        return self.fake_ospf_lsas
---
+++ tests/test_simple.py
@@ -0,0 +1,20 @@
+import unittest
+import test_merger
+import fibbingnode.shapeshifter.ospf_simple as smpl
+
+
+class TestSimple(test_merger.MergerTestCase):
+    def __init__(self, *args, **kwargs):
+        super(TestSimple, self).__init__(*args, **kwargs)
+        self.solver_provider = smpl.OSPFSimple
+
+    def _test(self, igp_topo, fwd_dags, expected_lsa_count):
+        solver = self.solver_provider()
+        lsas = solver.solve(igp_topo, fwd_dags)
+        self.assertTrue(test_merger.check_fwd_dags(fwd_dags,
+                                                   igp_topo,
+                                                   lsas,
+                                                   solver))
+
+if __name__ == '__main__':
+    unittest.main()
